185053875c87b8a877b312a17df5878d6eeed550
Kyle Fullen <kfullen@fb.com>
2019-07-08 16:22:59 -0700

    app icon and launch screen icon added

--- instagram/AppDelegate.m
+++ instagram/AppDelegate.m
@@ -21,10 +21,22 @@
     ParseClientConfiguration *config = [ParseClientConfiguration   configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
         
         configuration.applicationId = @"kfullen_instagram";
-        configuration.server = @"http://kfullen_instagram.herokuapp.com/parse";
+        configuration.server = @"http://kyle-instagram.herokuapp.com/parse";
     }];
     
     [Parse initializeWithConfiguration:config];
+    
+    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+    gameScore[@"score"] = @1337;
+    gameScore[@"playerName"] = @"Sean Plott";
+    gameScore[@"cheatMode"] = @NO;
+    [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            NSLog(@"Object saved!");
+        } else {
+            NSLog(@"Error: %@", error.description);
+        }
+    }];
     
     return YES;
 }

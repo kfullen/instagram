ec8de0d894f82ea88ea39cf3843e8c35628a6052
Kyle Fullen <kfullen@fb.com>
2019-07-09 11:47:31 -0700

    caching user works

--- instagram/AppDelegate.m
+++ instagram/AppDelegate.m
@@ -25,19 +25,14 @@
     }];
     
     [Parse initializeWithConfiguration:config];
-    /*
-    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
-    gameScore[@"score"] = @1337;
-    gameScore[@"playerName"] = @"Sean Plott";
-    gameScore[@"cheatMode"] = @NO;
-    [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-        if (succeeded) {
-            NSLog(@"Object saved!");
-        } else {
-            NSLog(@"Error: %@", error.description);
-        }
-    }];
-    */
+    
+    // check if there is a current user
+    if (PFUser.currentUser) {
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        
+        self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"HomeNavigationController"];
+        NSLog(@"at home screen");
+    }
     return YES;
 }
 
